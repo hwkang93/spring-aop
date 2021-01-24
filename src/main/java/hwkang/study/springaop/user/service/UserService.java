89e62199a6bec5b8fef35a36fbf5f61d3b6a4890
hwkang <landman93@naver.com>
2021-01-24 23:50:00 +0000

    Spring AOP Exception 관련 테스트 코드 작성중

--- src/main/java/hwkang/study/springaop/user/service/UserService.java
+++ src/main/java/hwkang/study/springaop/user/service/UserService.java
@@ -3,6 +3,7 @@ package hwkang.study.springaop.user.service;
 import hwkang.study.springaop.user.data.UserDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
 
