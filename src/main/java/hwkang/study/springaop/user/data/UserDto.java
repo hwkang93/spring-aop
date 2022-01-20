bd11821a5bcfc545d51562180cfcbd1c20f9f6ff
KHW <landman93@naver.com>
2022-01-20 14:19:37 +0900

    test

---
+++ src/main/java/hwkang/study/springaop/user/data/UserDto.java
@@ -0,0 +1,11 @@
+package hwkang.study.springaop.user.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserDto {
+    long userId;
+    String userName;
+}
