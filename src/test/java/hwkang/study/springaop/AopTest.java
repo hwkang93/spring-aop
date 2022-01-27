aaa5c9bb01aad7045a3dd4bba5b5b1ae50d1eae1
KHW <landman93@naver.com>
2022-01-27 12:41:32 +0900

    테스트케이스 추가

--- src/test/java/hwkang/study/springaop/AopTest.java
+++ src/test/java/hwkang/study/springaop/AopTest.java
@@ -6,22 +6,36 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-public class MethodInfoAopTest {
+public class AopTest {
 
     @Autowired
     UserService userService;
 
     @Test
+    @DisplayName("사용자 아이디가 1인 사용자의 이름은 khw 이다")
+    void findUser() {
+        final long userId = 1L;
+
+        UserDto userDto = userService.findByUserId(userId);
+
+        assertThat(userDto.getUserName()).isEqualTo("khw");
+    }
+
+    @Test
     @DisplayName("@Before 어노테이션 결과 확인")
     void printLogTest() {
+
         userService.findByUserId(1);
     }
 
     @Test
     @DisplayName("사용자가 없는 경우 NullPointerException 발생")
     void throwNullPointerException() {
-        userService.findByUserId(4);
+
+        assertThrows(NullPointerException.class, () -> userService.findByUserId(4));
     }
 }
